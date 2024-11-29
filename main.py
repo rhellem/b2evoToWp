3d5aa35c2a6395ee5f7dedc643a23391d94e3cd5
Rune Hellem <rune.hellem@gmail.com>
2024-11-29 21:15:40 +0100

    Added .gitignore, made use of myCommons.modules.envMgr in main.py and added required requirements.txt

---
+++ main.py
@@ -0,0 +1,11 @@
+from myCommons.modules.envMgr import EnvSetupManager
+
+def main():
+    envManager = EnvSetupManager()
+    envManager.setup_environment()
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
